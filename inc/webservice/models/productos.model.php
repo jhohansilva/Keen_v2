80746249d10b137cf6d02f2f74d1e4443eef2134
jhohansilva <jhohanf.silva@gmail.com>
2019-05-08 21:42:55 -0500

    Cambios model Productos

--- inc/webservice/models/productos.model.php
+++ inc/webservice/models/productos.model.php
@@ -5,23 +5,23 @@ class productos_model
 
     public function get_info_producto_mdl($item)
     {
-        $url = 'http://45.58.34.159/ecomerce/pedidos/dlls/M1001.DLL';
+        // $url = 'http://45.58.34.159/ecomerce/pedidos/dlls/M1001.DLL';
 
-        $data = array('datosh' => '800223811|1|0G10092S37077');
+        // $data = array('importarhtml' => '800223811|1|0G10092S37077');
 
-        $options = array(
-            'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method' => 'POST',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        if ($result === false) { /* Handle error */}
+        // $options = array(
+        //     'http' => array(
+        //         'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        //         'method' => 'POST',
+        //         'content' => http_build_query($data),
+        //     ),
+        // );
+        // $context = stream_context_create($options);
+        // $result = file_get_contents($url, false, $context);
+        // if ($result === false) { /* Handle error */}
 
-        // var_dump($result);
-        return $result;
-        // return $item . 'JH';
+        // // var_dump($result);
+        // return $result;
+        return $item . 'JH';
     }
 }
